73a0419dbf24aa79651ffaeee3f77c00fd88793c
Stephanie Obeytek <sobeytek@gmail.com>
2018-06-23 19:03:32 -0500

    Chapter 1 complete - Made Middle Earth

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -41,24 +41,33 @@ const lands = [
 // Chapter 1
 // ============
 const makeMiddleEarth = () => {
-
+  const $body = $("body");
   // HINT: Make a console.log for each of your functions to make sure that, when you click, the correct function is being called!
-
   console.log("Trying to make middle earth.");
-
   // 1. create a section tag with an id of middle-earth
-
+  const $section = $("<section/>");
+  $section.attr("id", "middle-earth");
+  
   // 2. append the section to the body of the DOM.
+  $("body").append($section);
 
   // 3. use a for loop to iterate over the lands array that does the following:
+  for(let i = 0; i < lands.length; i++){
+    //console.log(lands[i])
+//   3a. creates an article tag (there should be one for each land when the loop is done)
+    const $article = $("<article/>");
+//   3b. gives each land article an `id` tag of the corresponding land name
+    $article.attr("id", lands[i]);
+     console.log($article)
+//   3c. includes an h1 with the name of the land inside each land article
+    const $h1 = $('<h1>');
+    $h1.text(lands[i]);
+    $article.append($h1);
 
-  //   3a. creates an article tag (there should be one for each land when the loop is done)
+//   3d. appends each land to the middle-earth section
+     $section.append($article);
+  }
 
-  //   3b. gives each land article an `id` tag of the corresponding land name
-
-  //   3c. includes an h1 with the name of the land inside each land article
-
-  //   3d. appends each land to the middle-earth section
 
 };
 
